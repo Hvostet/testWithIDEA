4195dfd88161fef36cdd316b2426b55d73965bce
Hvostet <Hvostet@bk.ru>
2023-08-03 16:12:15 +0300

    2 12 first normal commit
    2 12 second normal commit

--- src/test2/test12.java
+++ src/test2/test12.java
@@ -6,5 +6,6 @@ public class test12 {
     public static void main(String[] args) {
 
         System.out.println("First normal commit");
+        System.out.println("Second normal commit");
     }
 }
